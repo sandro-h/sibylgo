519dd457a315ea82e068eed39028c2eafa1208ef
Sandro Heinzelmann <sandro.heinzelmann@gmail.com>
2020-11-29 19:45:02 +0100

    Replace deprecated request lib with node-fetch

--- vscode_ext/src/client.ts
+++ vscode_ext/src/client.ts
@@ -1,79 +1,35 @@
-import * as request from 'request';
+import fetch from 'node-fetch';
 
-export function formatTodos(restUrl: string, text: string): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-        request.post(
-            {
-                headers: {'content-type' : 'text/plain'},
-                url:     `${restUrl}/format`,
-                body:    Buffer.from(text).toString('base64')
-              },
-              (error, response, body: string) => {
-                if (error || response.statusCode !== 200) {
-                    reject(`HTTP ${response.statusCode} ${error}`);
-                }
-                else if (!body) {
-                    reject('No response body');
-                }
-                else {
-                    resolve(body.split(/\r?\n/));
-                }
-            }
-        );
-    });
+export async function formatTodos(restUrl: string, text: string): Promise<string[]> {
+    const res = await fetch(
+        `${restUrl}/format`,
+        {
+            method: 'POST',
+            headers: {'content-type' : 'text/plain'},
+            body: Buffer.from(text).toString('base64')
+        }
+    );
+    const body = await res.text();
+    return body.split(/\r?\n/);
 }
 
-export function foldTodos(restUrl: string, text: string): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-        request.post(
-            {
-                headers: {'content-type' : 'text/plain'},
-                url:     `${restUrl}/folding`,
-                body:    Buffer.from(text).toString('base64')
-              },
-              (error, response, body: string) => {
-                if (error || response.statusCode !== 200) {
-                    reject(`HTTP ${response.statusCode} ${error}`);
-                }
-                else if (!body) {
-                    reject('No response body');
-                }
-                else {
-                    resolve(body.split(/\r?\n/));
-                }
-            }
-        );
-    });
+export async function foldTodos(restUrl: string, text: string): Promise<string[]> {
+    const res = await fetch(
+        `${restUrl}/fold`,
+        {
+            method: 'POST',
+            headers: {'content-type' : 'text/plain'},
+            body: Buffer.from(text).toString('base64')
+        }
+    );
+    const body = await res.text();
+    return body.split(/\r?\n/);
 }
 
-export function cleanTodos(restUrl: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        request.post(
-            `${restUrl}/clean`,
-            (error, response) => {
-                if (error || response.statusCode !== 200) {
-                    reject(`HTTP ${response.statusCode} ${error}`);
-                }
-                else {
-                    resolve();
-                }
-            }
-        );
-    });
+export async function cleanTodos(restUrl: string): Promise<void> {
+    await fetch(`${restUrl}/clean`, { method: 'POST' });
 }
 
-export function trashTodos(restUrl: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        request.post(
-            `${restUrl}/trash`,
-            (error, response) => {
-                if (error || response.statusCode !== 200) {
-                    reject(`HTTP ${response.statusCode} ${error}`);
-                }
-                else {
-                    resolve();
-                }
-            }
-        );
-    });
+export async function trashTodos(restUrl: string): Promise<void> {
+    await fetch(`${restUrl}/trash`, { method: 'POST' });
 }
